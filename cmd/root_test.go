0bf5528ce5019e579958f44ab075ad29b3a27942
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2026-08-20 19:10:43 +0300

    chore: prepare v0.6.0

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -10,7 +10,7 @@ import (
 func TestRootCommandVersion(t *testing.T) {
 	cmd := newRootCmd(history.Store{}, nil, nil, 25*time.Minute, 5*time.Minute)
 
-	if cmd.Version != "v0.5.0" {
-		t.Fatalf("got version %q, want %q", cmd.Version, "v0.5.0")
+	if cmd.Version != "v0.6.0" {
+		t.Fatalf("got version %q, want %q", cmd.Version, "v0.6.0")
 	}
 }
